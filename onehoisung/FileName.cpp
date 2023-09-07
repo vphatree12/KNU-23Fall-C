cf007237943b0aec161c11cfdc2b5ced0953f766
vphatree12 <vphatree12@smail.kongju.ac.kr>
2023-09-07 10:36:24 +0900

    Update FileName.cpp

--- onehoisung/FileName.cpp
+++ onehoisung/FileName.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	printf("KNU!\n");
-	printf("SW!\n");
+	int num, floor, a, b;
+	scanf_s("%d", &num);
+	printf("%d층 피라미드\n", num);
 
+	for (a = 1; a <= num; a++) {
+		for (floor = 1; floor <= (num - a); floor++) {
+			printf(" ");
+		}
+		for (b = 1; b <= (a * 2) - 1; b++)
+			printf("*");
+		printf("\n");
+	}
 	return 0;
 }
