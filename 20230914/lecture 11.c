18eb3a21a356415555f4df814ae8fbf5e2125211
vphatree12 <vphatree12@smail.kongju.ac.kr>
2023-09-14 12:16:23 +0900

    Update lecture 11.c

--- 20230914/lecture 11.c
+++ 20230914/lecture 11.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-	int a = 1;
-	while (a <= 10) {
-		printf("%d´Ü\n",a);
-		for (int k = 1; k < 10; k++) {
-			int r = k * a;
-			printf("%d * %d = %d\n", a, k, r);
+	int a = 2;
+	while (a < 10) {
+		if (a != 5){
+			printf("%d´Ü\n",a);
+			for (int k = 1; k < 10; k++) {
+				int r = k * a;
+				printf("%d * %d = %d\n", a, k, r);
+			}
 		}
 		a++;
 	}
