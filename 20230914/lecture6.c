76e29ff4e4bba2cb0189673864b72d143eca2f22
vphatree12 <vphatree12@smail.kongju.ac.kr>
2023-09-14 11:15:22 +0900

    Create lecture6.c

---
+++ 20230914/lecture6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	scanf_s("%d", &a);
+	if (a > 0)
+		printf("a는 양의 정수입니다");
+	else if (a = 0)
+		printf("a는 0입니다");
+	else
+		printf("a는 음의 정수입니다");
+	return 0;
+}
