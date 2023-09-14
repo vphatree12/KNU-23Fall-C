37cfa41f23d715cbe20c1bae1923fcbd97248667
vphatree12 <vphatree12@smail.kongju.ac.kr>
2023-09-14 11:57:48 +0900

    Update lecture9.c

--- 20230914/lecture9.c
+++ 20230914/lecture9.c
@@ -5,9 +5,9 @@ int main() {
 	int sum = 0;
 	int val = 1;
 	for (i = 0; i < 10; i++) {
-		if (i % 2 == 0) continue;
-
-		printf("i = %d sum = %d\n", i, sum);
+		printf("i = %d val = %d sum = %d\n", i, val, sum);
+		sum += val;
+		val += 1;
 	}
 	printf("sum = %d\n", sum);
 	return 0;
