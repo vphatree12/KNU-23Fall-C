0faf637d966f777e82cf0d7d2c939376a25f8f76
vphatree12 <vphatree12@smail.kongju.ac.kr>
2023-09-21 11:18:25 +0900

    Create lecture4.c

---
+++ 20230921/lecture4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main() {
+	double x = 0, y = 0;
+	int count = 0, circle = 0;
+
+	srand(time(NULL));
+
+	while (count < 10000000) {
+		x = (double)rand() / (double)RAND_MAX;
+		y = (double)rand() / (double)RAND_MAX;
+		count++;
+	}
+	//난수 생성 함수
+}
