9119187c70426c0f57c97d61c2f3301b2a9c298d
vphatree12 <vphatree12@smail.kongju.ac.kr>
2023-09-21 12:00:26 +0900

    Update lecture4.c

--- 20230921/lecture4.c
+++ 20230921/lecture4.c
@@ -4,20 +4,38 @@
 #include <math.h>
 
 int main() {
-	double x = 0, y = 0;
+	double x = 0, y = 0, ㅠ = 0;
 	int count = 0, circle = 0;
-	int checkpoint 10000000;
+	int checkpoint = 10000000, progress = 0;
+	
 	srand(time(NULL));
 
 	while (count < 1000000000) {
 		x = (double)rand() / (double)RAND_MAX;
 		y = (double)rand() / (double)RAND_MAX;
 		count++;	//난수 생성 함수
-
-		if (count % checkpoint == 0) {
-
+		
+		double point = x * x + y * y;
+		if (point <= 1.0) {
+			circle++;
+			ㅠ = 4.0 * (double)circle / count;
 		}
+		double k;
+		if (count % checkpoint == 0) {
+			k = (double)circle / count;
 
+			int nemo = progress / 5;
+
+			// 5 % 단위 진행 상태 보기
+			for (int n = 0; n < 20; n++) {
+				if (n < nemo)
+					printf("■");
+				else
+					printf("□");
+			}
+			printf("\n");
+			// 5% 단위 진행 상태 보기
+		}
 	}
-	
+	return 0;
 }
