a2e3a5026e38686dc7c971a037877697d661b0fa
vphatree12 <vphatree12@smail.kongju.ac.kr>
2023-10-12 10:17:32 +0900

    Create coding1.c

---
+++ 20231012/coding1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int a = 3;
+	int* ptr1 = &a;
+	char b = "b";
+	int* ptr2 = &b;
+	printf("%d\n", ptr1);
+	printf("%d\n", sizeof(ptr1));
+	printf("%d\n", ptr2);
+	printf("%d\n", sizeof(ptr2));
+	return 0;
+}
