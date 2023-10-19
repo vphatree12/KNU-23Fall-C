8a7d887fba07cdc9f09588f0754e4e46cd495f58
vphatree12 <vphatree12@smail.kongju.ac.kr>
2023-10-19 11:30:43 +0900

    Update lecture2.c

--- 20231019/lecture2.c
+++ 20231019/lecture2.c
@@ -8,12 +8,12 @@ int main() {
 	printf("학생 수 입력 : ");
 	scanf_s("%d", &n);
 	s = (struct Student*)malloc(n * sizeof(struct Student));
-	for (int i = 1; i <= n; i++) {
-		printf("학생 # %d-%d 학번 입력 : ", n, i);
+	for (int i = 0; i < n; i++) {
+		printf("학생 # %d-%d 학번 입력 : ", n, i+1);
 		scanf_s("%d", &(s[i].sno));
-		printf("학생# %d-%d 이름 입력 : ", n, i);
+		printf("학생# %d-%d 이름 입력 : ", n, i+1);
 		scanf_s("%s", s[i].name, 10);
-		printf("학생# %d-%d 성적 입력 : ", n, i);
+		printf("학생# %d-%d 성적 입력 : ", n, i+1);
 		scanf_s("%d", &(s[i].score));
 	}
 	int sum = 0;
